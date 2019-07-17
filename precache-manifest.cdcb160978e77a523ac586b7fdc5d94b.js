bcbfaf30bd435c488c87cdc578428da3cb7f7756
Khyati Goyal <17uec065@lnmiit.ac.in>
2019-07-17 21:05:55 +0530

    Updates

---
+++ precache-manifest.cdcb160978e77a523ac586b7fdc5d94b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5104925360b065f8093a5ab2ca55f1b4",
+    "url": "/videos/index.html"
+  },
+  {
+    "revision": "5699217838d9bf30d2f6",
+    "url": "/videos/static/css/main.1e284ad4.chunk.css"
+  },
+  {
+    "revision": "7e6ac1444ef9242ebc00",
+    "url": "/videos/static/js/2.824d5390.chunk.js"
+  },
+  {
+    "revision": "5699217838d9bf30d2f6",
+    "url": "/videos/static/js/main.a9bb370e.chunk.js"
+  },
+  {
+    "revision": "39e39b46196f171b2925",
+    "url": "/videos/static/js/runtime~main.52b05483.js"
+  }
+]);
